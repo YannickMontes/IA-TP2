e4064b7c6ff230a457ac46c4626e8f9552adfc7c
MONTES Yannick <montes.yannick@gmail.com>
2017-04-12 11:57:02 +0200

    Add position class & start implementing Attributes with initialization

--- src/pacman/environnementRL/EtatPacmanMDPClassic.java
+++ src/pacman/environnementRL/EtatPacmanMDPClassic.java
@@ -10,13 +10,59 @@ import environnement.Etat;
  * Classe pour définir un etat du MDP pour l'environnement pacman avec QLearning tabulaire
 
  */
-public class EtatPacmanMDPClassic implements Etat , Cloneable{
+
+class Position
+{
+	private int x;
+	private int y;
+
+	public Position(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+	public void setX(int x)
+	{
+		this.x = x;
+	}
+
+	public int getY()
+	{
+		return y;
+	}
+
+	public void setY(int y)
+	{
+		this.y = y;
+	}
+
+	public int getX()
+	{
+		return this.x;
+	}
+}
+
+public class EtatPacmanMDPClassic implements Etat , Cloneable
+{
+	private Position pacmanPosition;
+	private ArrayList<Position> ghostPositions;
+
 
 	
-	public EtatPacmanMDPClassic(StateGamePacman _stategamepacman){
-	
-		
-		
+	public EtatPacmanMDPClassic(StateGamePacman _stategamepacman)
+	{
+		pacmanPosition = new Position(_stategamepacman.getPacmanState(0).getX(),
+										_stategamepacman.getPacmanState(0).getY());
+		ghostPositions = new ArrayList<>();
+		for(int i=0; i < _stategamepacman.getNumberOfGhosts(); i++)
+		{
+			Position ghostTmp = new Position(_stategamepacman.getGhostState(i).getX(),
+										_stategamepacman.getGhostState(i).getY());
+			ghostPositions.add(ghostTmp);
+		}
+
+		//_stategamepacman.get
 	}
 	
 	@Override
@@ -24,8 +70,13 @@ public class EtatPacmanMDPClassic implements Etat , Cloneable{
 		
 		return "";
 	}
-	
-	
+
+	@Override
+	public int hashCode()
+	{
+		return super.hashCode();
+	}
+
 	public Object clone() {
 		EtatPacmanMDPClassic clone = null;
 		try {
